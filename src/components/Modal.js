751d80856f47d060a824a6ff799c6f268c23f79f
Sajad Abedi <sajadxabedi@gmail.com>
2019-05-11 11:57:03 +0430

    added delete stream with modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom";
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">the content</div>
+    <div className="ui dimmer modals visible active" onClick={props.onDismiss}>
+      <div
+        className="ui standard modal visible active"
+        onClick={e => e.stopPropagation()}
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
     </div>,
     document.getElementById("modal")
   );
