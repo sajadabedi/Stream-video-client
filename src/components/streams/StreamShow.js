75f8728179b45cef5609fd3738330a1d375a24cc
Sajad Abedi <sajadxabedi@gmail.com>
2019-05-12 18:17:31 +0430

    installed flvjs pkg and wirig up flv to the page

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from "flv.js";
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
@@ -13,6 +18,7 @@ class StreamShow extends Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
